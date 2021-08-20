76e65cddd8df4e1a8ac865a093c4074744de5fde
MrAlbino <mralbino54@hotmail.com>
2021-08-20 16:05:19 +0300

    train.py update

--- src/train.py
+++ src/train.py
@@ -5,12 +5,14 @@ import glob
 import numpy as np
 import torch.nn as nn
 import torch.optim as optim
-
+import tqdm
+import torch
+import cv2
 ######### PARAMETERS ##########
 valid_size = 0.3
-test_size  = 0.1
+test_size  = 0.3
 batch_size = 4
-epochs = 20
+epochs = 5
 cuda = False
 input_shape = (224, 224)
 n_classes = 2
@@ -37,7 +39,6 @@ image_mask_check(image_path_list, mask_path_list)
 
 # SHUFFLE INDICES
 indices = np.random.permutation(len(image_path_list))
-
 # DEFINE TEST AND VALID INDICES
 test_ind  = int(len(indices) * test_size)
 valid_ind = int(test_ind + len(indices) * valid_size)
@@ -54,6 +55,10 @@ valid_label_path_list = mask_path_list[test_ind:valid_ind]
 train_input_path_list = image_path_list[valid_ind:]
 train_label_path_list = mask_path_list[valid_ind:]
 
+with open("losses.txt", "a") as file_object:
+    file_object.write("Batch Size:{} Epoch Size:{}  Image Count:{}".format(batch_size,epochs,len(train_input_path_list)))
+    file_object.write("\n")
+
 # DEFINE STEPS PER EPOCH
 steps_per_epoch = len(train_input_path_list)//batch_size
 
@@ -67,27 +72,50 @@ optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 # IF CUDA IS USED, IMPORT THE MODEL INTO CUDA
 if cuda:
     model = model.cuda()
+val_losses=[]
+train_losses=[]
 
 
 # TRAINING THE NEURAL NETWORK
+
 for epoch in range(epochs):
     running_loss = 0
+    #In each epoch, images and masks are mixed randomly in order not to output images sequentially.
+    pair_IM=list(zip(train_input_path_list,train_label_path_list))
+    np.random.shuffle(pair_IM)
+    unzipped_object=zip(*pair_IM)
+    zipped_list=list(unzipped_object)
+    train_input_path_list=list(zipped_list[0])
+    train_label_path_list=list(zipped_list[1])
+    
     for ind in range(steps_per_epoch):
         batch_input_path_list = train_input_path_list[batch_size*ind:batch_size*(ind+1)]
+        #train_input_path_list [0: 4] gets first 4 elements on first entry
+        #in the second loop train_input_list [4: 8] gets the second 4 elements
+        #element advances each time until batch_size
         batch_label_path_list = train_label_path_list[batch_size*ind:batch_size*(ind+1)]
         batch_input = tensorize_image(batch_input_path_list, input_shape, cuda)
         batch_label = tensorize_mask(batch_label_path_list, input_shape, n_classes, cuda)
+        #Our data that we will insert into the model in the preprocess section is prepared by entering the parameters.
+        
+        optimizer.zero_grad()#gresets the radian otherwise accumulation occurs on each iteration
+        # Manually reset gradients after updating Weights
 
-        optimizer.zero_grad()
+        outputs = model(batch_input) # Give the model batch_input as a parameter and assign the resulting output to the variable.
+        
 
-        outputs = model(batch_input)
+        # Forward passes the input data
         loss = criterion(outputs, batch_label)
-        loss.backward()
-        optimizer.step()
+        loss.backward()# Calculates the gradient, how much each parameter needs to be updated
+        optimizer.step()# Updates each parameter according to the gradient
 
-        running_loss += loss.item()
+        running_loss += loss.item()# loss.item () takes the scalar value held in loss.
+
         print(ind)
+        #validation 
         if ind == steps_per_epoch-1:
+            
+            train_losses.append(running_loss)
             print('training loss on epoch {}: {}'.format(epoch, running_loss))
             val_loss = 0
             for (valid_input_path, valid_label_path) in zip(valid_input_path_list, valid_label_path_list):
@@ -95,7 +123,44 @@ for epoch in range(epochs):
                 batch_label = tensorize_mask([valid_label_path], input_shape, n_classes, cuda)
                 outputs = model(batch_input)
                 loss = criterion(outputs, batch_label)
-                val_loss += loss
+                val_loss += loss.item()
+                val_losses.append(val_loss)
                 break
 
             print('validation loss on epoch {}: {}'.format(epoch, val_loss))
+    with open("losses.txt", "a") as file_object:
+    # Append 'hello' at the end of file
+        file_object.write("{}.Epoch => Train Loss: {} Validation Loss:{}".format(epoch,running_loss,val_loss))
+        file_object.write("\n")
+with open("losses.txt", "a") as file_object:
+    file_object.write("\n")
+torch.save(model, 'model.pt')
+print("Model Saved!")
+best_model = torch.load('model.pt')
+
+
+test_data_path='../data/test_data'
+test_data = glob.glob(os.path.join(test_data_path, '*'))
+def predict(test_input_path_list):
+
+    for i in tqdm.tqdm(range(len(test_input_path_list))):
+        batch_test = test_input_path_list[i:i+1]
+        test_input = tensorize_image(batch_test, input_shape, cuda)
+        outs = model(test_input)
+        out=torch.argmax(outs,axis=1)
+        out_cpu = out.cpu()
+        outputs_list=out_cpu.detach().numpy()
+        mask=np.squeeze(outputs_list,axis=0)
+            
+            
+        img=cv2.imread(batch_test[0])
+        mg=cv2.resize(img,(224,224))
+        mask_ind   = mask == 1
+        cpy_img  = mg.copy()
+        mg[mask==0 ,:] = (255, 0, 125)
+        opac_image=(mg/2+cpy_img/2).astype(np.uint8)
+        predict_name=batch_test[0]
+        predict_path=predict_name.replace('test_data', 'predicted_masked')
+        cv2.imwrite(predict_path,opac_image.astype(np.uint8))
+
+predict(test_data)
